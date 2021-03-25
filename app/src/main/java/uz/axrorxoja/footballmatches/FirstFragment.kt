db6d03b8c70185517e692c4b54c40f4459c5c6cf
axrorxoja@yandex-team.ru <4568325524YTyt65>
2021-03-25 11:22:42 +0500

    Add modules

---
+++ app/src/main/java/uz/axrorxoja/footballmatches/FirstFragment.kt
@@ -0,0 +1,40 @@
+package uz.axrorxoja.footballmatches
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.axrorxoja.footballmatches.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
